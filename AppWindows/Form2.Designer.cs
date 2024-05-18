6895924ff18470825cc81825e909dcebb843a229
Z4NO <danixt35@gmail.com>
2024-05-18 12:00:40 +0200

    Nuevas Funcionalidades Gráficas

--- AppWindows/Form2.Designer.cs
+++ AppWindows/Form2.Designer.cs
@@ -32,13 +32,21 @@
             this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Apellidos = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.REGISTRADO = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Email = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.REGISTRADO = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -48,12 +56,12 @@
             this.Nombre,
             this.Apellidos,
             this.ID,
-            this.REGISTRADO});
-            this.dataGridView1.Location = new System.Drawing.Point(745, 25);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.REGISTRADO,
+            this.Email});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 116);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(549, 455);
+            this.dataGridView1.Size = new System.Drawing.Size(652, 370);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -81,13 +89,27 @@
             this.ID.ReadOnly = true;
             this.ID.Width = 125;
             // 
+            // REGISTRADO
+            // 
+            this.REGISTRADO.HeaderText = "REGISTRAR";
+            this.REGISTRADO.MinimumWidth = 6;
+            this.REGISTRADO.Name = "REGISTRADO";
+            this.REGISTRADO.Width = 125;
+            // 
+            // Email
+            // 
+            this.Email.HeaderText = "Email";
+            this.Email.Name = "Email";
+            this.Email.ReadOnly = true;
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(160, 131);
+            this.label1.Location = new System.Drawing.Point(42, 222);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(251, 31);
+            this.label1.Size = new System.Drawing.Size(208, 26);
             this.label1.TabIndex = 1;
             this.label1.Text = "Nombre del Alumno";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -96,64 +118,134 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(160, 245);
+            this.label2.Location = new System.Drawing.Point(42, 315);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(251, 31);
+            this.label2.Size = new System.Drawing.Size(210, 26);
             this.label2.TabIndex = 2;
             this.label2.Text = "Apellido del Alumno";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(444, 131);
+            this.textBox1.Location = new System.Drawing.Point(255, 222);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(264, 31);
+            this.textBox1.Size = new System.Drawing.Size(199, 26);
             this.textBox1.TabIndex = 3;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(444, 245);
+            this.textBox2.Location = new System.Drawing.Point(255, 315);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(264, 31);
+            this.textBox2.Size = new System.Drawing.Size(199, 26);
             this.textBox2.TabIndex = 4;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(350, 346);
+            this.button1.Location = new System.Drawing.Point(184, 397);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(137, 35);
+            this.button1.Size = new System.Drawing.Size(103, 28);
             this.button1.TabIndex = 5;
             this.button1.Text = "Añadir";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // REGISTRADO
+            // panel1
             // 
-            this.REGISTRADO.HeaderText = "REGISTRAR";
-            this.REGISTRADO.MinimumWidth = 6;
-            this.REGISTRADO.Name = "REGISTRADO";
-            this.REGISTRADO.Width = 125;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Location = new System.Drawing.Point(578, 20);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(652, 488);
+            this.panel1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.Location = new System.Drawing.Point(553, 22);
+            this.button2.Name = "button2";
+            this.button2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button2.Size = new System.Drawing.Size(96, 54);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "buscar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(92, 33);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(434, 33);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(3, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 33);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Buscar:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Enviar Email",
+            "Dar de Baja"});
+            this.comboBox3.Location = new System.Drawing.Point(12, 20);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 26);
+            this.comboBox3.TabIndex = 9;
+            this.comboBox3.Text = "Funciones";
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(358, 62);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1924, 719);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1443, 584);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.dataGridView1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form2";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -162,14 +254,21 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Apellidos;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Apellidos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
         private System.Windows.Forms.DataGridViewButtonColumn REGISTRADO;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Email;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button button3;
     }
 }
--- AppWindows/Form2.cs
+++ AppWindows/Form2.cs
@@ -8,6 +8,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.ApplicationServices;
+using System.Net.Mail;
+using System.Net;
+
+
 
 namespace AppWindows
 {
@@ -26,9 +32,65 @@ namespace AppWindows
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            var row = dataGridView1.Rows[e.RowIndex];
+            var id = row.Cells["ID"].Value.ToString();
+
             
 
+            //CONSULTA SQL UPDATE DE RESGISTRADO
+            try
+            {
+                
+
+                // Crear la consulta SQL con parámetros
+                string Insert = "UPDATE users SET Registrado = true WHERE ID = @id";
+                MySqlCommand cmd = new MySqlCommand(Insert, conexionConnection);
+                if (row.Cells["Registrado"].Value.ToString() == "True")
+                {
+                    MessageBox.Show("El alumno ya se encuentra registrado");
+                    return;
+                }
+                else
+                {
+                    string EMIAL = Interaction.InputBox("Ingrese el email del alumno con ID = " + id, "Email", "", -1, -1);
+                    if (EMIAL == null || EMIAL == "")
+                    {
+                        MessageBox.Show("No se ha ingresado un email");
+                        return;
+                    }
+                    else
+                    {
+                        //Para actualizar el email
+                        conexionConnection.Open();
+                        string InsertEmail = "UPDATE users SET Email = @Email WHERE ID = @id";
+                        MySqlCommand cmdEmail = new MySqlCommand(InsertEmail, conexionConnection);
+                        cmdEmail.Parameters.AddWithValue("@Email", EMIAL);
+                        cmdEmail.Parameters.AddWithValue("@id", id);    
+                        cmdEmail.ExecuteNonQuery();
+                        row.Cells["Email"].Value = EMIAL;
+                        conexionConnection.Close();
+                        //Actualizar el email en el usuario
+                        list.Find(x => x.Id == Convert.ToInt32(id)).SetEmail = EMIAL;
+                        //Actualizar el estado de registrado
+                        conexionConnection.Open();
+                        cmd.Parameters.AddWithValue("@id", id);
+                        cmd.ExecuteNonQuery();
+                        conexionConnection.Close();
+
+                        row.Cells["Registrado"].Value = true;
+                        row.Cells["Registrado"].Style.BackColor = Color.Green;
+
+                        MessageBox.Show("Se ha registrado el alumno con ID = " + id);
+                    }  
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show("No se pudo realizar la siguiente operacion por el siguiente motivo =  " + ex.Message);
+            }
+
         }
+
+       
         private void Form2_FormClosed(Object sender, FormClosedEventArgs e)
         {
             //Por si queremos notifiacr al usuario de que se ha cerrado la ventana
@@ -69,8 +131,10 @@ namespace AppWindows
                         string nombre = reader["Nombre"].ToString();
                         string apellido = reader["Apellido"].ToString();
                         int id = Convert.ToInt32(reader["ID"]);
+                        Boolean registrado =  Convert.ToBoolean(reader["Registrado"]);
+                        dynamic email = reader["Email"].ToString();
 
-                        Alumno alum = new Alumno(nombre, apellido, id);
+                        Alumno alum = new Alumno(nombre, apellido, id, registrado, email);
                         list.Add(alum);
 
                         int n = dataGridView1.Rows.Add();
@@ -78,6 +142,25 @@ namespace AppWindows
                         dataGridView1.Rows[n].Cells[0].Value = alum.Name;
                         dataGridView1.Rows[n].Cells[1].Value = alum.Apelliddo;
                         dataGridView1.Rows[n].Cells[2].Value = alum.Id;
+                        dataGridView1.Rows[n].Cells[3].Value = alum.Registrado;
+                        if (dataGridView1.Rows[n].Cells[3].Value.ToString() == "True")
+                        {
+                            dataGridView1.Rows[n].Cells[3].Style.BackColor = Color.Green;
+                        }
+                        else
+                        {
+                            dataGridView1.Rows[n].Cells[3].Style.BackColor = Color.Red;
+                        }
+                        dataGridView1.Rows[n].Cells[4].Value = alum.email;
+                        if(dataGridView1.Rows[n].Cells[4].Value.ToString() == "")
+                        {
+                            dataGridView1.Rows[n].Cells[4].Value = "No tiene email";
+                        }
+                        else
+                        {
+                            dataGridView1.Rows[n].Cells[4].Value = alum.email;
+                        }
+                        
                     }
 
                 }
@@ -91,6 +174,10 @@ namespace AppWindows
             {
                 MessageBox.Show("Error al obtener datos: " + ex.Message);
             }
+        }
+        private void Limpiar()
+        {
+            this.dataGridView1.Rows.Clear();
         }
 
         private void Form2_Load(object sender, EventArgs e)
@@ -151,6 +238,8 @@ namespace AppWindows
             {
                 MessageBox.Show("No se pudo realizar la siguiente operacion por el siguiente motivo =  " + ex.Message);
             }
+            Limpiar();
+            CargarUsuarios();
         }
         private void SetId(Alumno al)
         {
@@ -166,6 +255,150 @@ namespace AppWindows
             }
 
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String busqueda = textBox3.Text;
+            if (busqueda == "")
+            {
+                dataGridView1.Rows.Clear();
+                CargarUsuarios();
+            }
+            else
+            {
+                List <Alumno> alumno_encontrados = list.FindAll(x => x.Name == busqueda);
+                if (alumno_encontrados == null)
+                {
+                    MessageBox.Show("No se ha encontrado un alumno con ID = " + busqueda);
+                    return;
+                }
+                else
+                {
+                   foreach(Alumno al in alumno_encontrados)
+                   {
+                        dataGridView1.Rows.Clear();
+                        dataGridView1.Rows.Add(al.Name, al.Apelliddo, al.Id, al.Registrado, al.email);
+                   }
+                   
+                }
+            }   
+        }
+        private void Enviar_Email(String body)
+        {
+            String user = Interaction.InputBox("Ingrese el nombre o ID del alumno para enciar el emial", "Enviar Email", "", -1, -1);
+            String asunto = Interaction.InputBox("Ingrese el asunto del email", "Asunto", "", -1, -1); 
+            String emsg = "Error al enviar el email, pómgase en contacto con el provedor del software";
+            String from = "s7200084@gmail.com";
+            String displayName = "Sistema de registro de alumnos";
+            try
+            {
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(from, displayName);
+                mail.To.Add(list.Find(x => x.Name == user || x.Id.ToString() == user).email);
+                mail.Subject = asunto;
+                mail.Body = body;
+                mail.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                smtp.Credentials = new NetworkCredential(from, "Baloncesto2005");
+                smtp.EnableSsl = true;
+                smtp.Send(mail);
+                MessageBox.Show("Se ha enviado el email al alumno con nombre o ID = " + user);
+            }catch(Exception ex)
+            {
+                MessageBox.Show(emsg + " = " + ex.Message);
+            }
+        }
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Nos permite enviar los emails a todos los alumnos registrados
+            if (comboBox3.Text == "Dar de Baja")
+            {
+                String serach = Interaction.InputBox("Ingrese el nombre o ID del alumno a dar de baja", "Dar de baja", "", -1, -1);
+                try
+                {
+                    conexionConnection.Open();
+                    string delete = "DELETE FROM users WHERE Nombre = @nombre OR ID = @id";
+                    MySqlCommand cmd = new MySqlCommand(delete, conexionConnection);
+                    cmd.Parameters.AddWithValue("@nombre", serach);
+                    cmd.Parameters.AddWithValue("@id", serach);
+                    cmd.ExecuteNonQuery();
+                    conexionConnection.Close();
+                    dataGridView1.Rows.Clear();
+                    CargarUsuarios();
+                    MessageBox.Show("Se ha dado de baja al alumno con nombre o ID = " + serach);
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("No se pudo realizar la siguiente operacion por el siguiente motivo =  " + ex.Message);
+                }
+            }
+            else if (comboBox3.Text == "Enviar Email")
+            {
+                String body = @"<!DOCTYPE html>
+                                <html lang='es'>
+                                <head>
+                                    <meta charset='UTF-8'>
+                                    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+                                    <title>Bienvenido a la Aplicación</title>
+                                    <style>
+                                        body {
+                                            font-family: Arial, sans-serif;
+                                            background-color: #f4f4f4;
+                                            margin: 0;
+                                            padding: 0;
+                                        }
+                                        .container {
+                                            max-width: 600px;
+                                            margin: 20px auto;
+                                            background-color: #ffffff;
+                                            padding: 20px;
+                                            border: 1px solid #dddddd;
+                                            border-radius: 5px;
+                                            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                                        }
+                                        h1 {
+                                            color: #333333;
+                                        }
+                                        p {
+                                            color: #555555;
+                                        }
+                                        .footer {
+                                            margin-top: 20px;
+                                            text-align: center;
+                                            color: #999999;
+                                            font-size: 12px;
+                                        }
+                                    </style>
+                                </head>
+                                <body>
+                                    <div class='container'>
+                                        <h1>¡Bienvenido a Nuestra Aplicación!</h1>
+                                        <p>Hola,</p>
+                                        <p>Nos complace informarte que has sido dado de alta en nuestra aplicación. Tu correo electrónico registrado es: <strong>email</strong></p>
+                                        <p>Si tienes alguna pregunta o necesitas ayuda, no dudes en contactarnos.</p>
+                                        <p>¡Gracias por unirte a nosotros!</p>
+                                        <div class='footer'>
+                                            <p>Este es un mensaje automático, por favor no respondas a este correo.</p>
+                                        </div>
+                                    </div>
+                                </body>
+                                </html>
+                                ";
+                Enviar_Email(body);
+            }
+        }
     }
 }
 public class Alumno
@@ -174,6 +407,7 @@ public class Alumno
     private int _id { get; set; }
     private String _apellido { get; set; }
     private Boolean _registraddo { get; set; }
+    dynamic _email { get; set; }    
 
 
     public Alumno(String name, String apellido)
@@ -189,10 +423,29 @@ public class Alumno
         this._registraddo = false;
         this._id = id;
     }
+    public Alumno(String name, String apellido, int id, Boolean registrado, dynamic email)
+    {
+        this._name = name;
+        this._apellido = apellido;
+        this._registraddo = registrado;
+        this._id = id;
+        this._email = email;
+    }
 
     public String Name { get { return _name; } }
     public String Apelliddo { get { return _apellido; } }
     public int Id { get { return _id; } }
+    public Boolean Registrado {  get { return _registraddo; } }
+    public dynamic email { get { return _email; } }
+
+    public dynamic SetEmail
+    {
+        get => _email;
+        set
+        {
+            _email = value;
+        }
+    }
 
     public int SetId
     {
